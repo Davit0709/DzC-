d2ebbf66c982bb8b829e07eab829454244fc1b4a
Davit <dav0709@icloud.com>
2023-03-08 14:54:26 +0400

    test1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите первое число: ");
+        int a = int.Parse(Console.ReadLine());
+        
+        Console.Write("Введите второе число: ");
+        int b = int.Parse(Console.ReadLine());
+        
+        int max = Math.Max(a, b);
+        int min = Math.Min(a, b);
+        
+        Console.WriteLine("Максимальное число: " + max);
+        Console.WriteLine("Минимальное число: " + min);
+    }
+}
+
